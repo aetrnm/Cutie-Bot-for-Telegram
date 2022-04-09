0791c9020f0d04c7f16214d565cd3387556236c0
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-09 22:55:06 +0200

    register command added

--- tools.ts
+++ tools.ts
@@ -1,4 +1,8 @@
 import { Bot } from "grammy";
+import * as groups from "./database.json";
+import * as fs from "fs";
+import { ICuteGroup } from "./ICuteGroup";
+import { ICuteUser } from "./ICuteUser";
 
 export async function setupBotCommandsDescription(bot: Bot): Promise<void> {
     await bot.api.setMyCommands([
@@ -12,6 +16,10 @@ export async function setupBotCommandsDescription(bot: Bot): Promise<void> {
             command: "register",
             description:
                 "Register in the game for the cutiest person of the day",
+        },
+        {
+            command: "info",
+            description: "Get information about the bot",
         },
     ]);
 }
@@ -28,11 +36,54 @@ export function setupBotCommands(bot: Bot) {
     });
 
     bot.command("register", (ctx) => {
+        const userID: number = ctx.from.id;
+        const groupID: number = ctx.chat.id;
+        const dbGroups: Array<ICuteGroup> = groups;
+
+        const newUser: ICuteUser = { id: userID, cuteCounter: 0 };
+        const newGroup: ICuteGroup = {
+            id: groupID,
+            groupMembers: [newUser],
+        };
+
+        const chatIndexInDB: number = dbGroups.findIndex(
+            (group) => group.id === groupID
+        );
+
+        if (chatIndexInDB === -1) {
+            dbGroups.push(newGroup);
+            ctx.reply("Group DOESN'T exists. Group created. User added");
+            const json = JSON.stringify(dbGroups);
+            fs.writeFile("database.json", json, "utf8", () =>
+                console.log("Data written successfully")
+            );
+            return;
+        }
+
+        const group: ICuteGroup = dbGroups[chatIndexInDB];
+        const userInGroup = group.groupMembers.findIndex(
+            (user) => user.id === userID
+        );
+
+        if (userInGroup === -1) {
+            group.groupMembers.push(newUser);
+            dbGroups[chatIndexInDB] = group;
+            ctx.reply("Group exists. User added");
+            const json = JSON.stringify(dbGroups);
+            fs.writeFile("database.json", json, "utf8", () =>
+                console.log("Data written successfully")
+            );
+
+            return;
+        }
+
+        ctx.reply("You are already registered!");
+    });
+
+    bot.command("info", (ctx) => {
         ctx.reply(
-            "Successfully registered " +
-                ctx.from.id +
-                "for a chat " +
-                ctx.chat.id
+            "Bot created by @aetrnm \nGithub repository: https://github.com/aetrnm/TGbot/tree/master",
+            { disable_web_page_preview: true }
         );
     });
 }
