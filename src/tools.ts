e606ef061e3ebd8e8c108f4582c2e22a6c068d67
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-10 21:11:18 +0200

    refactor: split code into functions

--- src/tools.ts
+++ src/tools.ts
@@ -1,12 +1,11 @@
 import { Bot } from "grammy";
-import * as groups from "./database.json";
 import * as fs from "fs";
 import { ICuteGroup } from "./ICuteGroup";
 import { ICuteUser } from "./ICuteUser";
+import chalk from "chalk";
 
 export async function setupBotCommandsDescription(bot: Bot): Promise<void> {
     await bot.api.setMyCommands([
-        { command: "start", description: "Start the bot" },
         { command: "help", description: "Show all avialable commands" },
         {
             command: "cutie",
@@ -24,56 +23,86 @@ export async function setupBotCommandsDescription(bot: Bot): Promise<void> {
     ]);
 }
 
-export function setupBotCommands(bot: Bot) {
-    bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+function registerGroup(groupID: number) {
+    const rawdata = fs.readFileSync(
+        new URL("./database.json", import.meta.url),
+        "utf8"
+    );
+    const groups: Array<ICuteGroup> = JSON.parse(rawdata.toString());
+    const newGroup: ICuteGroup = {
+        id: groupID,
+        groupMembers: [],
+    };
 
+    const groupIndexInDB: number = groups.findIndex(
+        (group) => group.id === groupID
+    );
+
+    if (groupIndexInDB === -1) {
+        groups.push(newGroup);
+        ctx.reply("Group DIDN'T exist. Group created.");
+        const json = JSON.stringify(groups);
+        fs.writeFile("database.json", json, "utf8", () =>
+            console.log(
+                chalk.green("New group added. Data written successfully.")
+            )
+        );
+
+        return;
+    }
+}
+
+function addUserToGroup(groupID: number, userID: number) {
+    const newUser: ICuteUser = { id: userID, cuteCounter: 0 };
+
+    const rawdata = fs.readFileSync(
+        new URL("./database.json", import.meta.url),
+        "utf8"
+    );
+    const groups: Array<ICuteGroup> = JSON.parse(rawdata.toString());
+    const groupIndexInDB: number = groups.findIndex(
+        (group) => group.id === groupID
+    );
+    groups[groupIndexInDB].groupMembers.push(newUser);
+    const json = JSON.stringify(groups);
+    fs.writeFile("./database.json", json, "utf8", () =>
+        console.log(chalk.green("New user added. Data written successfully."))
+    );
+}
+
+export function setupBotCommands(bot: Bot) {
     bot.command("help", (ctx) => {
         ctx.reply("*Help* \n_List of commands_:", { parse_mode: "MarkdownV2" });
     });
-
     bot.command("cutie", (ctx) => {
-        ctx.reply("Today the cutie is @aetrnm");
+        ctx.reply("Today the cutie is @Conterry");
     });
 
     bot.command("register", (ctx) => {
-        const userID: number = ctx.from.id;
-        const groupID: number = ctx.chat.id;
-        const dbGroups: Array<ICuteGroup> = groups;
+        const userID = ctx.from.id;
+        const groupID = ctx.chat.id;
+        const rawdata = fs.readFileSync(
+            new URL("./database.json", import.meta.url),
+            "utf8"
+        );
+        const groups: Array<ICuteGroup> = JSON.parse(rawdata.toString());
 
-        const newUser: ICuteUser = { id: userID, cuteCounter: 0 };
-        const newGroup: ICuteGroup = {
-            id: groupID,
-            groupMembers: [newUser],
-        };
-
-        const chatIndexInDB: number = dbGroups.findIndex(
+        const groupIndexInDB: number = groups.findIndex(
             (group) => group.id === groupID
         );
-
-        if (chatIndexInDB === -1) {
-            dbGroups.push(newGroup);
-            ctx.reply("Group DOESN'T exists. Group created. User added");
-            const json = JSON.stringify(dbGroups);
-            fs.writeFile("database.json", json, "utf8", () =>
-                console.log("Data written successfully")
-            );
+        if (groupIndexInDB === -1) {
+            registerGroup(groupID);
+            addUserToGroup(groupID, userID);
             return;
         }
 
-        const group: ICuteGroup = dbGroups[chatIndexInDB];
+        const group: ICuteGroup = groups[groupIndexInDB];
         const userInGroup = group.groupMembers.findIndex(
             (user) => user.id === userID
         );
 
         if (userInGroup === -1) {
-            group.groupMembers.push(newUser);
-            dbGroups[chatIndexInDB] = group;
-            ctx.reply("Group exists. User added");
-            const json = JSON.stringify(dbGroups);
-            fs.writeFile("database.json", json, "utf8", () =>
-                console.log("Data written successfully")
-            );
-
+            addUserToGroup(groupID, userID);
             return;
         }
 
