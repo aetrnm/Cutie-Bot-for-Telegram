a58cb7bbab3687630cbe00a88ee1c0a8c048af83
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-12 13:48:07 +0200

    refactor: add file extension

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import { Bot } from "grammy";
-import { setupBotCommands } from "./setupBotCommands";
-import { setupBotCommandsDescription } from "./setupBotCommandsDescription";
+import { setupBotCommands } from "./setupBotCommands.js";
+import { setupBotCommandsDescription } from "./setupBotCommandsDescription.js";
 import * as dotenv from "dotenv";
 const env = dotenv?.config()?.parsed || { BOT_TOKEN: "" };
 const BOT_TOKEN = env.BOT_TOKEN;
