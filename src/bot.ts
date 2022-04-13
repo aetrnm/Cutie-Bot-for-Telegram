ddeb7e163db06a66d3edfd3e484736f99d5adae3
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-04-13 23:52:41 +0200

    refactor: simplified dotenv parse

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,9 @@
 import { Bot } from "grammy";
 import { setupBotCommands } from "./setupBotCommands.js";
 import { setupBotCommandsDescription } from "./setupBotCommandsDescription.js";
-import * as dotenv from "dotenv";
-const env = dotenv?.config()?.parsed || { BOT_TOKEN: "" };
-const BOT_TOKEN = env.BOT_TOKEN;
+import { config } from "dotenv";
+config();
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const bot: Bot = new Bot(BOT_TOKEN);
 
